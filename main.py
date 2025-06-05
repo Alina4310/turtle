35388ca960a97efd29ed660223bf58db85aa6cc6
Alina4310 <alina.ivanova11.11@mail.ru>
2025-06-05 11:11:04 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+from turtle import *
+
+shape("turtle")  # форма черепашки
+colormode(255)
+
+bgcolor("sienna")  # цвет фона
+speed(1)  # скорость черепашки
+
+pencolor("red")
+pensize(3)
+
+penup()
+forward(100)
+left(90)
+pendown()
+backward(50)
+right(135)
+forward(20)
+
+done()
+
+
+
+
