2e1b5dfa565a2a26b105bd3573c0ae8f4c8b6953
Alina4310 <alina.ivanova11.11@mail.ru>
2025-06-05 11:31:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -20,5 +20,58 @@ forward(20)
 done()
 
 
+from turtle import *
+
+shape("turtle")
+colormode(255)
+
+bgcolor("blue")
+speed(3)
+
+pencolor("red")
+pensize(3)
+
+forward(50)
+left(90)
+forward(50)
+left(90)
+forward(50)
+left(90)
+forward(50)
+
+penup()
+left(180)
+forward(50)
+
+pendown()
+right(30)
+forward(50)
+right(120)
+forward(50)
+
+penup()
+left(120)
+forward(130)
+
+pencolor("yellow")
+pendown()
+circle(30)
+
+penup()
+left(90)
+forward(30)
+
+for i in range(8):
+    penup()
+    forward(30)
+    pendown()
+    forward(50)
+    penup()
+    backward(80)
+    right(45)
+
+
+done()
+
 
 
